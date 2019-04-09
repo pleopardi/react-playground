c0d954fcd16810c101747b64522de9cffc494887
Paolo Leopardi <leopardipaolo@gmail.com>
2019-04-09 09:14:37 +0200

    feat(pdfGeneration):
    add business logic

--- src/experiments/pdfGeneration/App.js
+++ src/experiments/pdfGeneration/App.js
@@ -1,7 +1,45 @@
 import React from "react";
+import dayjs from "dayjs";
+import jsPDF from "jspdf";
+
+const textRows = Array.from({ length: 100 }, (_, index) => `Row ${index + 1}`);
+
+function generatePDF() {
+  const date = dayjs().format("DD-MM-YYYY, H.mm");
+  const fileName = `Report ${date}.pdf`;
+  const horizontalMargin = 15;
+  const pageHeight = 297;
+  const title = `Report ${date}`;
+  const verticalMargin = 15;
+
+  const pdf = new jsPDF("p", "mm", "a4");
+
+  // Add title
+  pdf.setFontSize(20);
+  pdf.text(horizontalMargin, verticalMargin, title);
+
+  // Setup body
+  pdf.setFontSize(11);
+  const lineHeight = pdf.getLineHeight();
+  let y = 2 * verticalMargin;
+
+  // Add body
+  textRows.forEach(textRow => {
+    if (y > pageHeight - lineHeight) {
+      y = verticalMargin;
+      pdf.addPage();
+    }
+
+    pdf.text(horizontalMargin, y, textRow);
+
+    y += lineHeight;
+  });
+
+  pdf.save(fileName);
+}
 
 function App() {
-  return <div>PDF Generation</div>;
+  return <button onClick={generatePDF}>Generate PDF</button>;
 }
 
 export default App;
