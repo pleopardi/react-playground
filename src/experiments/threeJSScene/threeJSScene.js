b8d331db08bf8091ce1535ee91734c936b16bbd4
Paolo Leopardi <leopardipaolo@gmail.com>
2019-04-20 11:41:03 +0200

    feat:
    add model, texture, controls, light

--- src/experiments/threeJSScene/threeJSScene.js
+++ src/experiments/threeJSScene/threeJSScene.js
@@ -1,13 +1,20 @@
 import {
-  BoxGeometry,
-  Mesh,
-  MeshBasicMaterial,
+  AmbientLight,
+  AxesHelper,
+  Box3,
+  Color,
+  DoubleSide,
+  MeshLambertMaterial,
+  ObjectLoader,
   PerspectiveCamera,
   Scene,
+  TextureLoader,
   WebGLRenderer
 } from "three";
+import { OrbitControls } from "./controls/OrbitControls";
 
 export default function threeJSScene(element) {
+  let scaleFactor;
   const initialXSize = 80;
   const initialYSize = 80;
   const initialZSize = 80;
@@ -24,26 +31,53 @@ export default function threeJSScene(element) {
       const { height, width } = element.getBoundingClientRect();
 
       scene = new Scene();
-      const camera = new PerspectiveCamera(75, width / height, 0.1, 1000);
+      scene.background = new Color(0xffffff);
+      const camera = new PerspectiveCamera(90, width / height, 0.1, 1000);
+
+      scene.add(new AxesHelper(100));
+
+      camera.position.z = 200;
 
       const renderer = new WebGLRenderer();
       renderer.setSize(width, height);
       element.appendChild(renderer.domElement);
 
-      const geometry = new BoxGeometry(1, 1, 1);
-      const material = new MeshBasicMaterial({ color: 0x00ff00 });
+      const material = new MeshLambertMaterial({
+        map: new TextureLoader().load(
+          "http://localhost:3000/assets/threejs/wall.jpg"
+        ),
+        side: DoubleSide
+      });
 
-      model = new Mesh(geometry, material);
+      const loader = new ObjectLoader();
 
-      scene.add(model);
+      loader.load(
+        // resource URL
+        "http://localhost:3000/assets/threejs/box.json",
 
-      camera.position.z = 5;
+        // onLoad callback
+        // Here the loaded data is assumed to be an object
+        function(object) {
+          model = object;
+          const boundingBox = new Box3().setFromObject(object);
+          const maxSize = Math.max(...Object.values(boundingBox.getSize()));
+
+          scaleFactor = 1 / maxSize;
+
+          object.scale.set(1, 1, 1);
+          object.rotation.set(0, -Math.PI / 3, 0);
+          object.position.set(0, 0, 0);
+          object.material = material;
+          scene.add(object);
+        }
+      );
+
+      new OrbitControls(camera, renderer.domElement);
+
+      scene.add(new AmbientLight(0xffffff, 2));
 
       function animate() {
         requestAnimationFrame(animate);
-
-        model.rotation.x += 0.01;
-        model.rotation.y += 0.01;
 
         renderer.render(scene, camera);
       }
@@ -56,9 +90,9 @@ export default function threeJSScene(element) {
       currentZSize = z;
 
       model.scale.set(
-        currentXSize / initialXSize,
-        currentYSize / initialYSize,
-        currentZSize / initialZSize
+        currentXSize * scaleFactor,
+        currentYSize * scaleFactor,
+        currentZSize * scaleFactor
       );
     }
   };
